2a9dfa19d13a8ba2cfa66e4d02e9356cc9ee0269
Mas'ud <68595786+dev-onimoe@users.noreply.github.com>
2024-07-06 14:44:18 +0100

    network commit

--- Sources/NetworkLibrary/NetworkLibrary.swift
+++ Sources/NetworkLibrary/NetworkLibrary.swift
@@ -1,2 +1,43 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class NetworkLibrary {
+    
+    public static let shared = NetworkLibrary()
+    
+    public func makeCall(url: String, completion: @escaping (Data?, String?) -> Void) {
+        
+        var query = url
+        print(query)
+        guard let url = URL(string: query) else {return}
+        let session = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            //Utility.hideProgressHUD()
+            if let err = error {
+                
+                completion(nil, err.localizedDescription)
+                print(err.localizedDescription)
+            }else {
+                
+                if let responseData = data {
+        
+                    completion(responseData, nil)
+                   
+                    
+                }else {
+                    completion(nil, "Unknown error")
+                }
+                
+            }
+        }).resume()
+    }
+    
+}
+
+public struct Response {
+    
+    var successful : Bool
+    var message: String?
+    var object : Any?
+}
